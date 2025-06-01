bccef6002402ac7261e513f945db4ed921cddb1f
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2025-06-02 01:05:45 +0500

    Update server.js

--- Dashboard/project-folder/server.js
+++ Dashboard/project-folder/server.js
@@ -310,9 +310,15 @@ app.delete('/api/orders/:id', async (req, res) => {
     }
 });
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB ulandi"))
-  .catch(err => console.error("Ulanishda xatolik:", err));
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { 
+  useNewUrlParser: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
 
 app.listen(PORT, () => {
     console.log(`Server http://localhost:${PORT} da ishlayapti ✅`);
